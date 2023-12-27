a4450571856a2f089c00e873571f1188f80c6ff3
PrathiSithu <sithuminiprathiba@gmail.com>
2023-12-27 14:38:23 +0530

    js commit

---
+++ jscript.js
@@ -0,0 +1,11 @@
+setInterval(() => {
+    let hh = document.getElementById('hh');
+    let mm = document.getElementById('mm');
+    let ss = document.getElementById('ss');
+
+    let sec_dot = document.querySelector('.sec_dot');
+    let min_dot = document.querySelector('.min_dot');
+    let hr_dot = document.querySelector('.hr_dot');
+
+    
+})
