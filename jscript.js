99a6add2cb29a88fc790bb805f860a8f8165d22c
PrathiSithu <sithuminiprathiba@gmail.com>
2023-12-27 15:43:05 +0530

    clock

--- jscript.js
+++ jscript.js
@@ -23,4 +23,31 @@ setInterval(() => {
     }
 
     h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+    ampm.innerHTML = am;
+
+    hh.style.strokeDashoffset = 510 - (510 * h) / 12;
+
+    mm.style.strokeDashoffset = 630 - (630 * m) / 60;
+
+    ss.style.strokeDashoffset = 760 - (760 * s) / 60;
+
+    sec_dot.style.transform = `rotateZ(${s * 6}deg)`;
+
+    min_dot.style.transform = `rotateZ(${m * 6}deg)`;
+
+    hr_dot.style.transform = `rotateZ(${h * 30}deg)`;
+});
+
+const body = document.querySelector("body"),
+    modeToggle = document.querySelector(".dark-light");
+
+modeToggle.addEventListener("click", () => {
+    modeToggle.classList.toggle("active");
+    body.classList.toggle("dark")
 })
